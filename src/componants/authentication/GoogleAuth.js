33b32117ba4ac29789dd5db87990ddd729e3cc51
tariqrasheed172001 <rasheedhadiq@gmail.com>
2023-07-29 19:44:46 +0530

    updated google login depency

--- src/componants/authentication/GoogleAuth.js
+++ src/componants/authentication/GoogleAuth.js
@@ -1,5 +1,5 @@
 import React from "react";
-import GoogleLogin from "react-google-login";
+import GoogleLogin from "@leecheuk/react-google-login";
 import GoogleButton from "react-google-button";
 import Cookies from "js-cookie";
 
