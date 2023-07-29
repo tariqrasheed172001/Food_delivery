47fe1725ce33fdfddc2d050830f43ac86e83fa95
tariqrasheed172001 <rasheedhadiq@gmail.com>
2023-07-29 20:28:18 +0530

    changed the logic for google auth

--- src/index.js
+++ src/index.js
@@ -1,42 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import Login from "./componants/authentication/Login"
-import Register from './componants/authentication/Register';
-import Home from './componants/Home/Home';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import { SnackbarProvider } from 'notistack';
-import Otp from './componants/OTP/Otp';
-import ResetPassword from './componants/authentication/ResetPassword';
-import PageNotFound from './componants/authentication/PageNotFound';
-import { gapi } from 'gapi-script';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Login from "./componants/authentication/Login";
+import Register from "./componants/authentication/Register";
+import Home from "./componants/Home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
+import Otp from "./componants/OTP/Otp";
+import ResetPassword from "./componants/authentication/ResetPassword";
+import PageNotFound from "./componants/authentication/PageNotFound";
+import { gapi } from "gapi-script";
 
 // this is the index file
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-   
-  <SnackbarProvider anchorOrigin={{vertical: 'top',horizontal:'center'}}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/send-otp" element={<Otp />}/>
-        <Route path="reset-password/:user_id/:token" element={<ResetPassword />} />
-        <Route path="/page-not-found" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </SnackbarProvider> 
-  
+      <SnackbarProvider
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/send-otp" element={<Otp />} />
+            <Route
+              path="reset-password/:user_id/:token"
+              element={<ResetPassword />}
+            />
+            <Route path="/page-not-found" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </SnackbarProvider>
+    ;
   </React.StrictMode>
 );
 
 gapi.load("client:auth2", () => {
   gapi.client.init({
-    clientId:
-      "*****.apps.googleusercontent.com",
+    clientId: "*****.apps.googleusercontent.com",
     plugin_name: "chat",
   });
 });
